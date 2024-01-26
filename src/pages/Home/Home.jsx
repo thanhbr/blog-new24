6a8243cda793b79481b71f50ece20200eae2ca05
ThanhMaThe <thanh.mathe@upos.vn>
2024-01-26 09:56:53 +0700

    update tailwind

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useDarkMode } from '../../context/DarkModeContext';
+import Header from '../../components/Header/Header';
 
 const Home = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   console.log('isDarkMode', isDarkMode);
+
   return (
-    <div>Home</div>
+    <Header />
   )
 }
 
